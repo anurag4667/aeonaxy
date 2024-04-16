2d60e54e4885231534bff69daba67279f642bb61
Anurag Rajput <arajput3000@gmail.com>
2024-04-16 16:23:18 +0530

    updated server

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const app = require("./app.js");
 const sql = require("./config/database.js");
 
 app.listen(process.env.PORT,()=>{
-    console.log(`server is running on port ${process.env.PORT}`);
+    console.log(`server is running on port ${process.env.PORT} in enviornment ${process.env.NODE_ENV}`);
 })
 
-
+app.get("/",(req,res)=>{
+    res.send("working");
+})
 
